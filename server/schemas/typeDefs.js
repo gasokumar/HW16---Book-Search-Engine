637530144e7c067f7c9c1895ed579839ce90909b
Ami Asokumar <gasokumar@gmail.com>
2021-12-29 07:10:29 -0500

    added mutation type as well as input savedBook

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,4 @@
-// typeDefs are what data the user can request.
+// typeDefs are what data the user can request, as well as the queries they can make.
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
@@ -30,10 +30,14 @@ const typeDefs = gql`
   }
 
   # Will need to create mutations for this app.
-
-  #   type Mutation {
-  #     createMatchup(tech1: String!, tech2: String!): Matchup
-  #     createVote(_id: String!, techNum: Int!): Matchup
-  #   }
+  type Mutation {
+    #   login takes email and password as parameters, returns an Auth type
+    login(email: String!, password: String!): Auth
+    # addUser takes in a username, email, and password, returns an Auth type
+    addUser(username: String!, email: String!, password: String!): Auth
+    # saveBook takes in an input, returns a User type.
+    saveBook(input: savedBook!): User
+    removeBook(bookId: ID!): User
+  }
 `;
 module.exports = typeDefs;
