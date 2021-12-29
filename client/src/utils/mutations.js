5ef35d5e03ac487105f95db8477c0c974900272c
Ami Asokumar <gasokumar@gmail.com>
2021-12-29 12:38:52 -0500

    created addUser mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,20 +1,40 @@
 import { gql } from "@apollo/client";
 import { login } from "../../../server/controllers/user-controller";
 // There should be four mutations from typeDefs.js
-// One for login, addUser, saveBook, and removeBook.
+// One for addUser, login, saveBook, and removeBook.
+
+// ADD_USER will execute the addUser mutation.
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String) {
+    addUser(username: $username, email: $email, password: $password) {
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+      token
+    }
+  }
+`;
 
 // LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
 
 // login(email: String!, password: String!): Auth
 
 // ^ From typeDefs.js
-export const LOGIN_USER = gql`
-mutation login {
+// export const LOGIN_USER = gql`
+// mutation login()
 
-}
-`;
-
-// ADD_USER will execute the addUser mutation.
+// `;
 
 // SAVE_BOOK will execute the saveBook mutation.
 
