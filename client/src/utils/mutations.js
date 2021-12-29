1c73cf4c5a8b4b399dfe9c6004f8e75f7fac34dd
Ami Asokumar <gasokumar@gmail.com>
2021-12-29 18:21:39 -0500

    refactored App.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -34,16 +34,15 @@ export const ADD_USER = gql`
 
 // ^ From typeDefs.js
 export const LOGIN_USER = gql`
-mutation login($email: String!, $password: String!) {
+  mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-        token
-        user {
-            _id
-        }
+      token
+      user {
+        _id
+      }
     }
-}
-
-// `;
+  }
+`;
 
 // SAVE_BOOK will execute the saveBook mutation.
 
